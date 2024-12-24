80b0ace48ecc4948a966689d3fbfd6eb237f14ee
tommyngx <tommyfederation@gmail.com>
2024-12-24 22:06:23 +1100

    save

--- mammo_classification/src/models/birads_classifier.py
+++ mammo_classification/src/models/birads_classifier.py
@@ -1,11 +1,13 @@
+import torch
 import torch.nn as nn
+import torchvision.models as models
+import timm
 
 class BiradsClassifier(nn.Module):
     def __init__(self, model_name='resnet50', num_classes=4, pretrained=True):
         super(BiradsClassifier, self).__init__()
         self.model_name = model_name
         
-        # Model architectures
         if 'resnet' in model_name:
             self.model = self._init_resnet(model_name, num_classes, pretrained)
         elif 'efficientnet' in model_name:
@@ -16,6 +18,38 @@ class BiradsClassifier(nn.Module):
             self.model = self._init_convnext(model_name, num_classes, pretrained)
         else:
             raise ValueError(f"Model {model_name} not supported")
+
+    def _init_resnet(self, model_name, num_classes, pretrained):
+        if model_name == 'resnet50':
+            model = models.resnet50(pretrained=pretrained)
+        elif model_name == 'resnet101':
+            model = models.resnet101(pretrained=pretrained)
+        model.fc = nn.Linear(model.fc.in_features, num_classes)
+        return model
+
+    def _init_efficientnet(self, model_name, num_classes, pretrained):
+        if model_name == 'efficientnet_b0':
+            model = models.efficientnet_b0(pretrained=pretrained)
+        elif model_name == 'efficientnet_b4':
+            model = models.efficientnet_b4(pretrained=pretrained)
+        model.classifier[1] = nn.Linear(model.classifier[1].in_features, num_classes)
+        return model
+
+    def _init_vit(self, num_classes, pretrained):
+        model = timm.create_model('vit_base_patch16_224', pretrained=pretrained)
+        model.head = nn.Linear(model.head.in_features, num_classes)
+        return model
+
+    def _init_convnext(self, model_name, num_classes, pretrained):
+        if model_name == 'convnext_tiny':
+            model = models.convnext_tiny(pretrained=pretrained)
+        elif model_name == 'convnext_base':
+            model = models.convnext_base(pretrained=pretrained)
+        model.classifier[2] = nn.Linear(model.classifier[2].in_features, num_classes)
+        return model
+
+    def forward(self, x):
+        return self.model(x)
 
     def train(self, train_loader, num_epochs):
         for epoch in range(num_epochs):
