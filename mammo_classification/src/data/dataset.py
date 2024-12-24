41c2007ec4435436fea5b42c5a58818971de47d0
tommyngx <tommyfederation@gmail.com>
2024-12-25 00:39:30 +1100

    yaml augment fix

--- mammo_classification/src/data/dataset.py
+++ mammo_classification/src/data/dataset.py
@@ -7,13 +7,13 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 class MammographyDataset(Dataset):
-    def __init__(self, metadata_path, split='train', task='birads', transform=None):
+    def __init__(self, metadata_path, split, task, config):
         """
         Args:
             metadata_path: Path to CSV file with columns [path, birads, lesion_types, split]
             split: 'train' or 'test'
             task: 'birads' or 'lesion'
-            transform: Optional transform to be applied
+            config: Configuration dictionary containing augmentation parameters
         """
         self.df = pd.read_csv(metadata_path)
         self.df = self.df[self.df['split'] == split]
@@ -28,14 +28,18 @@ class MammographyDataset(Dataset):
         self.num_classes = len(self.labels)
         self.label_to_idx = {label: idx for idx, label in enumerate(self.labels)}
         
-        if transform is None:
-            self.transform = A.Compose([
-                A.Resize(224, 224),
-                A.Normalize(),
-                ToTensorV2()
-            ])
-        else:
-            self.transform = transform
+        # Load augmentations from config
+        augmentation_config = config['data']['augmentation']
+        self.transform = A.Compose([
+            A.Resize(224, 224),
+            A.HorizontalFlip(p=augmentation_config['horizontal_flip']),
+            A.VerticalFlip(p=augmentation_config['vertical_flip']),
+            A.Rotate(limit=augmentation_config['rotation_range']),
+            A.RandomScale(scale_limit=augmentation_config['zoom_range']),
+            A.RandomBrightnessContrast(brightness_limit=augmentation_config['brightness_range']),
+            A.Normalize(),
+            ToTensorV2()
+        ])
 
     def __len__(self):
         return len(self.df)
